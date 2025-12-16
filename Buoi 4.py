b77d09b8113b5a65cfe278052719f327932b5036
25022021-lgtm <25022021@vnu.edu.vn>
2025-12-16 10:51:54 +0700

    Update Buoi 4.py

--- Buoi 4.py
+++ Buoi 4.py
@@ -82,4 +82,94 @@ while not palindrome(n):
     count += 1
 print("So buoc de bien n thanh so doi xung la:",count)
 print("So doi xung thu duoc la:",n)
+#W4A9
+n = int(input())
+a = []
+for i in range(1, n+1):
+    if i ** 0.5 == int(i ** 0.5):
+        a.append(i)
+print(*a)
+#W4A12
+a, b = map(int, input().split())
+print(int(a*(1.007**b)))
+#W4A11
+n = int(input())
+count = 0
+for i in range(2, (n+1)/2):
+    if n % i == 0 and i % 2 == 0:
+        count += 1
+print(count)
+#W4A13
+def close_pair(a, b):
+    sum = 1
+    for i in range(2, int(a/2+1)):
+        if a % i == 0:
+            sum += i
+    if sum == b:
+        return True
+    else:
+        return False
+x, y = map(int,input().split())
+if close_pair(x,y) and close_pair(y,x):
+    print("true")
+else:
+    print("false")
+#W4A14
+def gcd(a, b):
+    while b!= 0:
+        a, b = b, a % b
+    return a
+a, b = map(int, input().split())
+print(gcd(a, b))
+#W4A20
+n = int(input())
+while n % 2 == 0:
+    n //= 2
+if n == 1:
+    print("true")
+else:
+    print("false")
+#W4A23
+n = int(input())
+S = 0
+while S < n:
+    for k in range(1, n):
+        S += k
+        if S >= n:
+            print(k)
+            break
+#W4A24
+def check_password(password):
+    if len(password) <= 6:
+        return False
+    
+    has_upper = False   
+    has_lower = False   
+    has_digit = False   
+    has_special = False 
+    
+    for char in password:
+        if char.isupper():
+            has_upper = True
+        elif char.islower():
+            has_lower = True
+        elif char.isdigit():
+            has_digit = True
+        else:
+            has_special = True
+    if has_upper and has_lower and has_digit and has_special:
+        return True
+    else:
+        return False
+s = input()
+if check_password(s):
+    print("true")
+else:
+    print("false")
+#W4A34
+a = input()
+b = input()
+c = a.split(b)
+print(''.join(c))
 '''
+
